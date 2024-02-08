dd0f3c7e13549fc4616b7987be4fa20fe7337b31
PuniSuli <50169909+PuniSuli@users.noreply.github.com>
2024-02-08 14:02:06 +0200

    Update td.js

--- td/td.js
+++ td/td.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
   
-	Lampa.Utils.putScriptAsync(['https://raw.githubusercontent.com/PuniSuli/just/main/junk.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://punisuli.github.io/just/td/td/td.js'], function () { });
   
   })();
   
